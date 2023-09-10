a18875d8cbecde3cc9650c87d03b4238d6276725
LimWeiJen <limweijen96@gmail.com>
2023-09-10 14:58:16 +0800

    fix: Fixed some bugs
    bugs on orientation problems and bugs on game over state.

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,12 +1,16 @@
 import { Server } from 'Socket.IO'
 import { v4 as uuidv4 } from 'uuid';
 
+// map to store the data of various ongoing games
 var games = new Map();
+
+// keep track of the current game id (will increment after each game is created)
 var i = 0;
 
+// map to track connected players
 var ps = new Map();
  
-const SocketHandler = (req: any, res: any) => {
+const SocketHandler = (_: any, res: any) => {
   if (res.socket.server.io) {
     console.log('Socket is already running')
   } else {
@@ -16,19 +20,25 @@ const SocketHandler = (req: any, res: any) => {
 
     io.on('connection', socket => {
       socket.emit('connection')
+
+      // when the socket is connected by a player, generate a unique ID for the player
       var playerId = uuidv4();
       console.log(playerId + 'connected');
 
+      // triggers when the player clicks the "play" button on the client
       socket.on('join', () => {
+        // check if the player has clicked the "play" button before
         if (ps.has(playerId)) return;
         ps.set(playerId, true);
 
+        // check if there is an available game slot with less than 2 players
         if (games.has(i) && games.get(i).players < 2) {
           var g = games.get(i);
           g.players++;
           g.pid[1] = playerId;
           games.set(i, g);
         } else {
+          // if no available slot, create a new game
           i++;
           games.set(i, {
             players: 1,
@@ -42,23 +52,28 @@ const SocketHandler = (req: any, res: any) => {
 
         console.log(games.get(i));
 
+        // emit 'player' event to the current socket with player information
         socket.emit("player", {
           playerId, players, color, roomId: i
         })
       })
 
+      // Broadcast 'move' event to other connected sockets
       socket.on('move', (msg) => socket.broadcast.emit('move', msg));
 
+      // Broadcast 'play' event to other connected sockets
       socket.on("play", (msg) => socket.broadcast.emit('play', msg));
 
+      // Handle 'gameOver' event
       socket.on('gameOver', (roomId) => {
+        // Remove the game from the map
         games.delete(roomId);
+        // Broadcast 'gameOver' event to other connected sockets
         socket.broadcast.emit('gameOver', roomId);
       })
 
-      // if a user disconnects just print their playerID
+      // Handle 'disconnect' event
       socket.on('disconnect', function () {
-        //TODO: come up with a disconnect protocol
         console.log(playerId + ' disconnected');
       });
     })
--- pages/components/Chessboard.tsx
+++ pages/components/Chessboard.tsx
@@ -8,32 +8,49 @@ const game = new Chess();
 
 let color = "";
 let players;
-let play = true;
+let playing = false;
 let roomId = -1;
 
 const ChessBoard = () => {
-  const [fen, setfen] = useState("start");
-  const [orientation, setorientation] = useState<"white" | "black">("white");
-  const [waiting, setwaiting] = useState(false);
+  const [fen, setfen] = useState("start"); // current position of the chessboard
+  const [orientation, setorientation] = useState<"white" | "black">("white"); // orientation of the chess board
+  const [waiting, setwaiting] = useState(false); // flag indicating if the game is waiting for players
+  const [hidetimer, sethidetimer] = useState(false); // flag indicating whether to hide the timer
+
+  // function to join the game
+  const joinGame = () => {
+    if (roomId === -1) {
+      // if the user hasn't joined any games, join a new game
+      socket.emit("join");
+    } else {
+      // else reload the page
+      window.location.reload();
+    }
+  }
   
   useEffect(() => {
+    // fetch the socket endpoint
     fetch('/api/socket').then(() => {
-      socket = io();
+      socket = io(); // initialize the socket connection
 
+      // event listener for 'connection' event
       socket.on('connection', () => {
         console.log('connected');
       })
 
+      // event listener for 'player' event
       socket.on('player', (msg: any) => {
         color = msg.color;
         players = msg.players;
         roomId = msg.roomId;
         setorientation(msg.color);
 
+        // if there are two players in the game, start the game
         if (players == 2) {
           setwaiting(false);
           socket.emit('play', msg.roomId);
         } else {
+          // if only one player, set waiting to be true, indicating that we're waiting for another player to join the same game
           setwaiting(true);
         }
       })
@@ -41,30 +58,36 @@ const ChessBoard = () => {
       socket.on('move', (msg: any) => {
         if (msg.room == roomId) {
           try {
-            game.move(msg.move);
-            setfen(game.fen());
+            game.move(msg.move); // make the move on the chessboard
+            setfen(game.fen()); // update the fen state
           } catch (e) {}
         }
       })
 
+      // start the game
       socket.on('play', (msg: any) => {
         if (msg == roomId) {
           console.log('start')
-          play = false;
+          playing = true;
+          setwaiting(false);
+          sethidetimer(true);
         }
       })
 
+      // stop the game
       socket.on('gameOver', (msg: any) => {
-            if (msg == roomId) {
-              console.log("game over");
-              play = true;
-            }
-          })
+        if (msg == roomId) {
+          console.log("game over");
+          playing = false;
+          sethidetimer(false);
+        }
+      })
     })
   }, [])
 
+  // function to make a move
   function makeAMove(move: any) {
-    if (play || game.isGameOver() || game.turn() == 'w' && color == 'black' || game.turn() == 'b' && color == 'white') return;
+    if (!playing || game.isGameOver() || game.turn() == 'w' && color == 'black' || game.turn() == 'b' && color == 'white') return;
     try {
       game.move(move);
       setfen(game.fen());
@@ -98,7 +121,8 @@ const ChessBoard = () => {
     <Chessboard position={fen} onPieceDrop={onDrop} boardOrientation={orientation} boardWidth={500} />
     </div>
     <div className='menu'>
-    <button className='btn' onClick={() => socket.emit("join", 0)}>{waiting ? 'Loading...' : 'Play'}</button>
+    {hidetimer ? null :
+    <button className='btn' onClick={joinGame}>{waiting ? 'Loading...' : 'Play'}</button> }
     </div>
   </div>
   )
